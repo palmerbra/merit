e27ef21dd36b595bb7d16cc42a8591bba351b0ee
palmerbra <palmerbra@uwplatt.edu>
2017-05-05 01:14:40 -0500

    \

--- ford meme/ford meme/MainForm.vb
+++ ford meme/ford meme/MainForm.vb
@@ -3,7 +3,7 @@
 Public Class MainForm
    Dim webClient As New System.Net.WebClient
    Dim result As String
-   Dim credentials As System.Net.NetworkCredential
+   Dim credentials As New System.Net.NetworkCredential("", "")
    Dim ticker As String
 
    ' loose goose kind of meme here watch out 
@@ -16,6 +16,7 @@ Public Class MainForm
          txtFord.Text = result
       Catch ex As Exception
          MsgBox(ex.Message)
+         txtTicker.Enabled = False
       End Try
 
 
@@ -24,13 +25,12 @@ Public Class MainForm
    End Sub
 
    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-      credentials = New Net.NetworkCredential("518fc2d67f61480355fcb4fe6c054c4e", "73f6b6b78e8a52ca87091ab61e1dfae8")
-      webClient.Credentials = credentials
-      btnSave.Enabled = False
-      txtPass.Enabled = False
-      txtUser.Enabled = False
-      lblUser.Enabled = False
-      lblPass.Enabled = False
+      txtTicker.Enabled = False
+      'btnSave.Enabled = False
+      'txtPass.Enabled = False
+      'txtUser.Enabled = False
+      'lblUser.Enabled = False
+      'lblPass.Enabled = False
 
    End Sub
 
@@ -38,6 +38,8 @@ Public Class MainForm
 
    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
       credentials = New Net.NetworkCredential(txtUser.Text, txtPass.Text)
+      webClient.Credentials = credentials
+      txtTicker.Enabled = True
    End Sub
 
    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
